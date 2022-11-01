f3caf09caddbacf1ce3705ea7f8c4cfbee3885eb
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-01 18:34:45 +0100

    Done with Project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,47 @@ import Data from "./Data";
 
 function App() {
   const [index, setIndex] = useState(0);
+  // eslint-disable-next-line
   const [data, setData] = useState(Data);
+
+  // You can use this function instead of the below useEffect
+
+  /* const nextSlide = () => {
+     setIndex((oldValue) => {
+       let newIndex = oldValue + 1;
+
+       if (newIndex > data.length - 1) {
+         newIndex = 0;
+       }
+       return newIndex;
+     });
+   };
+
+   const prevSlide = () => {
+     setIndex((oldValue) => {
+       let newIndex = oldValue - 1;
+
+       if (newIndex < 0) {
+         newIndex = data.length - 1;
+       }
+       return newIndex;
+     });
+   };
+ */
+
+  // Note : whem using this method, your useEffect autoplay will run out of index
+  // To fix the gotcha, copy paste you
+
+  /*setIndex((oldValue) => {
+    let newIndex = oldValue + 1;
+
+    if (newIndex > data.length - 1) {
+      newIndex = 0;
+    }
+    return newIndex;
+  }); */
+
+  // To your setInterval function.
 
   useEffect(() => {
     //  Checkfor last person...
@@ -18,6 +58,17 @@ function App() {
       setIndex(0);
     }
   }, [index, data]);
+
+  // useEffect for autoplay
+  useEffect(() => {
+    // Always have a clean up function for your windows function
+    let Slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+
+    // always have a clean up function to make your app more functional and to avoid bugs
+    return () => clearInterval(Slider);
+  }, [index]);
 
   return (
     <section className="section">
