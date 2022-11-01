d490ef360ba4ea2d358adb9c8b12f52416517ce4
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-01 16:27:18 +0100

    Added another section

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ function App() {
 
   return (
     <section className="section">
-      <div className="title"></div>
+      <div className="title">
+        <h2>
+          <span>/ </span> Reviews
+        </h2>
+      </div>
+
+      <div className="section-center"></div>
     </section>
   );
 }
