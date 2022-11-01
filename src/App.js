b05ae77b7f8bdb6bb30772cc812a86f2cac1c8e5
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-01 16:25:21 +0100

    Added section

--- src/App.js
+++ src/App.js
@@ -7,22 +7,9 @@ function App() {
   const [data, setData] = useState(Data);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <FaChevronRight />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="title"></div>
+    </section>
   );
 }
 
