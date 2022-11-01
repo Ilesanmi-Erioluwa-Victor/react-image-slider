fe389479fd2032479d2fa8a814a5f677efd8e13b
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-01 16:19:33 +0100

    Added my state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import { FachevronRight } from "react-icons/fa";
+import Data from "./Data";
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const [data, setData] = useState(Data);
   return (
     <div className="App">
       <header className="App-header">
